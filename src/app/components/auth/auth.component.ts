c9f75ad1b45463ae86ecf241bec7644b6cdfd921
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2025-01-17 21:27:33 +0500

    added some auth routes

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { OtpCodeComponent } from './otp-code/otp-code.component';
+import { SignupComponent } from './signup/signup.component';
+import { CreatePasswordComponent } from './create-password/create-password.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [LoginComponent, FooterComponent, ResetPasswordComponent],
+  imports: [LoginComponent, FooterComponent, CreatePasswordComponent, ResetPasswordComponent, OtpCodeComponent, SignupComponent],
   templateUrl: './auth.component.html', 
   styleUrl: './auth.component.scss'
 })
---
+++ src/app/components/auth/otp-code/otp-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './otp-code.component.html',
+  styleUrl: './otp-code.component.scss'
+})
+export class OtpCodeComponent {
+
+}
